7739c70669333d10ee7e2135d71e727ac0818551
Shivani R <shivanir.1901219@srec.ac.in>
2021-09-03 15:40:03 +0530

    first commit

--- Rupees2Dollar/ContentView.swift
+++ Rupees2Dollar/ContentView.swift
@@ -8,10 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var rs:String = ""
+    @State var dollar:String = ""
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack{
+        TextField("Rupees",text:$rs)
             .padding()
+            Button(action: {
+                dollar = String(convert(rs: rs))
+            }, label: {
+                Text("Convert")
+            } )
+        TextField("Dollar",text:$dollar)
+            .padding()
+        
     }
+    }
+}
+
+func convert(rs:String )-> Double {
+    let rupees:Double? = Double(rs)
+    if (rupees !=  nil){
+        let dollar = rupees! / 73.0
+        return round(dollar*1000)/1000
+    }
+    return 0.0
 }
 
 struct ContentView_Previews: PreviewProvider {
