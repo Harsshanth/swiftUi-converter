03177260e4fffd5bdd4a1ff9476d6835c4fc5275
Shivani R <shivanir.1901219@srec.ac.in>
2021-09-03 15:10:39 +0530

    Initial Commit

---
+++ Rupees2Dollar/Rupees2DollarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rupees2DollarApp.swift
+//  Rupees2Dollar
+//
+//  Created by user on 03/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Rupees2DollarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
